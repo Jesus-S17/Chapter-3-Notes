e100c6f54040ecb3d0e306919f6c66049b6a29ed
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-03-23 10:29:42 -0700

    Practiced return

---
+++ array1.js
@@ -0,0 +1,18 @@
+//prints the second value in an array
+// function printSecond(arr) {
+//     console.log(arr[0]);
+// }
+// printSecond( ['a', 'b', 'c', 'd'] );
+
+//1
+// function printSecond(arr) {
+//     console.log(arr[0]);
+// }
+// printSecond( ['3', '4', '5', '6'] );
+
+//2
+function printSecond(arr) {
+    console.log(arr[5]);
+}
+printSecond( ['3', '4', '5', '6'] );
+(printSecond);
