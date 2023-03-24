6e0b0bd022b792024a40d40aae07c4ec6f8f2214
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-03-24 10:29:03 -0700

    Practiced array

--- array1.js
+++ array1.js
@@ -11,8 +11,64 @@
 // printSecond( ['3', '4', '5', '6'] );
 
 //2
-function printSecond(arr) {
-    console.log(arr[5]);
-}
-printSecond( ['3', '4', '5', '6'] );
-(printSecond);
+// function printSecond(arr) {
+//     console.log(arr[3]);
+// }
+// printSecond( ['3', '4', '5', '6'] );
+
+//3
+// function printSecond(arr) {
+//     if (' '){
+//         console.log("empty array");
+//     }
+//     console.log(arr[4]);
+// }
+// printSecond( ['3', '4', '5', '6',' '] );
+
+//4
+// function printSecond(arr) {
+//     console.log(arr[0]);
+//     console.log(arr[1]);
+//     console.log(arr[2]);
+//     console.log(arr[3]);
+// }
+// printSecond( ['3', '4', '5', '6'] );
+
+//5
+// function printSecond(arr) {
+//     console.log(arr[2]);
+//     console.log(arr[4]);
+//     console.log(arr[7]);
+//     console.log(arr[9]);
+// }
+// printSecond( ['3', '4', '5', '7', '8', '9', '10', '11', '12', '13'] );
+
+//6
+// function printSecond(arr) {
+//     console.log(arr[2]);
+//     console.log(arr[4]);
+//     console.log(arr[6]);
+//     console.log(arr[8]);
+// }
+// printSecond( ['2','3', '4', '5','6', '7', '8', '9', '10', '11', '12', '13'] );
+
+//7
+// function printSecond(arr) {
+//     console.log(arr[2]);
+//     console.log(arr[3]);
+//     console.log(arr[4]);
+//     console.log(arr[5]);
+//     console.log(arr[6]);
+//     console.log(arr[7]);
+//     console.log(arr[8]);
+//     console.log(arr[9]);
+//     console.log(arr[10]);
+//     console.log(arr[11]);
+// }
+// printSecond( ['2','3', '4', '5','6', '7', '8', '9', '10', '11', '12', '13'] );
+
+//8
+
+
+//9
+
