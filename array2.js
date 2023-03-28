6ea21b2a00e10fec7721792a7e0ef9c3ed11b89e
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-03-28 10:28:34 -0700

    Array notes

--- array2.js
+++ array2.js
@@ -1,4 +1,14 @@
+// 1
+// let nums = [1, 2, 3, 4, 5];
+// let newNums = [1, 2];
+// console.log('if n =5: ' + nums);
+// console.log('if n =2: ' + newNums);
+
+//2
 let nums = [1, 2, 3, 4, 5];
 let newNums = [1, 2];
+if (n = 0){
+
+}
 console.log('if n =5: ' + nums);
 console.log('if n =2: ' + newNums);
