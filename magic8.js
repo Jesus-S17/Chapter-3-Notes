66ef204ec686e15d583e546d809f1bd1b300fc95
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-04-17 10:07:39 -0700

    objectNotes

--- magic8.js
+++ magic8.js
@@ -4,5 +4,5 @@ let randomNum =  Math.floor(Math.random()*answerList.length);
 let randomAnswer =  answerList[randomNum];
 console.log(randomAnswer);
 
-console.log('want me to answer your prayers?')
+console.log('want me to answer your prayers?');
 let randomWRD = random
