1e32da18429b622d01ff18c9fa9fa0341e1707ff
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-04-21 10:30:12 -0700

    comboMenu

--- magic8.js
+++ magic8.js
@@ -1,8 +1,17 @@
-let answerList = ['yes', 'no', 'maybe'];
+const readLine = require(`readline-sync`);
 
-let randomNum =  Math.floor(Math.random()*answerList.length);
-let randomAnswer =  answerList[randomNum];
+let answer = readLine.question("Welcome to the great fortune place. Do you want to know your fortune? yes or no: ");
+while (answer != "yes" && answer != "no"){
+    console.log("invalid choice");
+    answer = readLine.question("Enter y or n: ");
+}
+let answer2 = readLine.question("Ask your question: ");
+
+
+
+let answerList = ['It is certain.', 'Without a doubt.', 'My reply is no.', 'My sources say no.', 'Ask again later.', 'Reply hazy, try again'];
+
+let randomNum = Math.floor(Math.random()*answerList.length);
+
+let randomAnswer = answerList[randomNum];
 console.log(randomAnswer);
-
-console.log('want me to answer your prayers?');
-let randomWRD = random
